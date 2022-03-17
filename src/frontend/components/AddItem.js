d1581fb1a81ed6e9f59afd1892ed1ec357e5e290
Blair Taylor <blair.taylor@gmail.com>
2022-03-16 22:53:21 -0400

    Added fields to additem screen

--- src/frontend/components/AddItem.js
+++ src/frontend/components/AddItem.js
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-import { ethers } from "ethers"
-import { Row, Form, Button } from 'react-bootstrap'
+import { ethers } from 'ethers'
+import { Row, Form, Button, Card } from 'react-bootstrap'
 import { create as ipfsHttpClient } from 'ipfs-http-client'
+import ticket1 from '../images/simple-party-ticket-1.png'
 const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
 
 const AddItem = ({ marketplace, nft }) => {
@@ -9,6 +10,7 @@ const AddItem = ({ marketplace, nft }) => {
   const [price, setPrice] = useState(null)
   const [name, setName] = useState('')
   const [description, setDescription] = useState('')
+  const [address, setAddress] = useState('')
   const uploadToIPFS = async (event) => {
     event.preventDefault()
     const file = event.target.files[0]
@@ -17,37 +19,43 @@ const AddItem = ({ marketplace, nft }) => {
         const result = await client.add(file)
         console.log(result)
         setImage(`https://ipfs.infura.io/ipfs/${result.path}`)
-      } catch (error){
-        console.log("ipfs image upload error: ", error)
+      } catch (error) {
+        console.log('ipfs image upload error: ', error)
       }
     }
   }
   const createNFT = async () => {
     if (!image || !price || !name || !description) return
-    try{
+    try {
       const timestamp = Date.now()
-      const result = await client.add(JSON.stringify({image, price, name, description, timestamp}))
+      const result = await client.add(
+        JSON.stringify({ image, price, name, description, timestamp }),
+      )
       mintThenList(result)
-    } catch(error) {
-      console.log("ipfs uri upload error: ", error)
+    } catch (error) {
+      console.log('ipfs uri upload error: ', error)
     }
   }
   const mintThenList = async (result) => {
     const uri = `https://ipfs.infura.io/ipfs/${result.path}`
-    // mint nft 
-    await(await nft.mint(uri)).wait()
-    // get tokenId of new nft 
+    // mint nft
+    await (await nft.mint(uri)).wait()
+    // get tokenId of new nft
     const id = await nft.tokenCount()
     // approve marketplace to spend nft
-    await(await nft.setApprovalForAll(marketplace.address, true)).wait()
+    await (await nft.setApprovalForAll(marketplace.address, true)).wait()
     // add nft to marketplace
     const listingPrice = ethers.utils.parseEther(price.toString())
-    await(await marketplace.makeItem(nft.address, id, listingPrice)).wait()
+    await (await marketplace.makeItem(nft.address, id, listingPrice)).wait()
   }
   return (
     <div className="container-fluid mt-5">
       <div className="row">
-        <main role="main" className="col-lg-12 mx-auto" style={{ maxWidth: '1000px' }}>
+        <main
+          role="main"
+          className="col-lg-12 mx-auto"
+          style={{ maxWidth: '1000px' }}
+        >
           <div className="content mx-auto">
             <Row className="g-4">
               <Form.Control
@@ -56,11 +64,79 @@ const AddItem = ({ marketplace, nft }) => {
                 name="file"
                 onChange={uploadToIPFS}
               />
-              <Form.Control onChange={(e) => setName(e.target.value)} size="lg" required type="text" placeholder="Name" />
-              <Form.Control onChange={(e) => setDescription(e.target.value)} size="lg" required as="textarea" placeholder="Description" />
-              <Form.Control onChange={(e) => setPrice(e.target.value)} size="lg" required type="number" placeholder="Price in ETH" />
+              <Form.Control
+                onChange={(e) => setName(e.target.value)}
+                size="lg"
+                required
+                type="text"
+                placeholder="Name"
+              />
+              <Form.Control
+                onChange={(e) => setDescription(e.target.value)}
+                size="lg"
+                required
+                as="textarea"
+                placeholder="Description"
+              />
+              <Form.Control
+                onChange={(e) => setName(e.target.value)}
+                size="lg"
+                required
+                type="text"
+                placeholder="Date"
+              />
+              <Form.Control
+                onChange={(e) => setName(e.target.value)}
+                size="lg"
+                required
+                type="text"
+                placeholder="Time"
+              />
+              <Form.Control
+                onChange={(e) => setAddress(e.target.value)}
+                size="lg"
+                required
+                type="text"
+                placeholder="Address"
+              />
+              <Form.Control
+                onChange={(e) => setPrice(e.target.value)}
+                size="lg"
+                required
+                type="number"
+                placeholder="Price in ETH"
+              />
+            </Row>
+            <Row className="g-4">
+              <Card
+                style={{ width: '50rem', paddingTop: 20, paddingBottom: 20 }}
+              >
+                <Card.Img variant="bottom" src={ticket1} />
+                <Card.ImgOverlay style={{paddingTop:40, paddingLeft: 540, fontFamily: 'Arial, Helvetica, sans-serif', fontWeight: 'bold'}}>
+                  <Card.Text>
+                      March 15, 2020
+                  </Card.Text>
+                  <Card.Text>
+                      8pm
+                  </Card.Text>
+                  <Card.Text>
+                      {address}
+                  </Card.Text>
+                  <Card.Text>
+                      {price} ETH
+                  </Card.Text>
+                </Card.ImgOverlay>
+              </Card>
+            </Row>
+            <Row className="g-4">
+              &nbsp;
               <div className="d-grid px-0">
-                <Button onClick={createNFT} variant="primary" size="lg" className="custom-btn">
+                <Button
+                  onClick={createNFT}
+                  variant="primary"
+                  size="lg"
+                  className="custom-btn"
+                >
                   Add an NFT and list for sale
                 </Button>
               </div>
@@ -69,7 +145,7 @@ const AddItem = ({ marketplace, nft }) => {
         </main>
       </div>
     </div>
-  );
+  )
 }
 
 export default AddItem
